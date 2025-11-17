9b3857b3c38f2dc20a67940e067366e795f2dbd0
Elle Jacobs <enj657@gmail.com>
2025-11-17 15:59:38 -0600

    Remove hardcoded Devise secret

--- backend/config/initializers/devise.rb
+++ backend/config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'REMOVED_SECRET'
+  config.secret_key = ENV["DEVISE_SECRET_KEY"] if ENV["DEVISE_SECRET_KEY"].present?
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
